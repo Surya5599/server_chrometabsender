5447aa26ea50b5c28660bc69454f982bec237857
Surya Singh <44600494+Surya5599@users.noreply.github.com>
2020-07-12 15:46:40 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+
+var express = require('express');
+
+var app = express();
+
+var server = app.listen(process.env.PORT || 3000);
+
+app.use(express.static('public'));
+
+console.log("sever is running");
+
+var socket = require('socket.io');
+
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+
+app.get('/', function(req, res) {
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('OK');
+});
+
+function newConnection(socket){
+  console.log('new Connection: ' + socket.id);
+  var ID = socket.id;
+  socket.on('openTab', openTab);
+  socket.on('disconnect', disConnect);
+
+  function openTab(data){
+    socket.broadcast.emit("tab", data);
+  }
+
+
+  function disConnect(socket){
+    console.log('Disconnected Connection: ' + ID);
+  }
+}
